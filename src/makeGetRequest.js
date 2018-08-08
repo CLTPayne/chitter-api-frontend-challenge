890c8eb5e13de16392a9916e79cece3f88bf3a4a
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-08 11:57:29 +0100

    Separate helper function for XMLHttpRequest
    * Function to be passed into controller

---
+++ src/makeGetRequest.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function makeGetRequest(url, onSuccess) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readystate < 4) {
+      return;
+    }
+    if (xhr.status !== 200) {
+      return;
+    }
+    var responseJSON = JSON.parse(xhr.responseText);
+    onSuccess(responseJSON);
+  };
+  xhr.open("GET", url, true);
+  xhr.send("");
+};
